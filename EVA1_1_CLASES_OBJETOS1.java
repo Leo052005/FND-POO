f28778fd210364e5a765689b9f4c43abb67c167d
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2024-02-07 22:43:18 -0600

    Add files via upload

---
+++ EVA1_1_CLASES_OBJETOS1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_clases_objetos1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA1_1_CLASES_OBJETOS1 {
+
+    public static void main(String[] args) {
+     int valor = 100;
+     persona perso1 = new Scanner (System.in);
+        System.out.println("perso 1");
+    }
+}
+class persona{
+}
