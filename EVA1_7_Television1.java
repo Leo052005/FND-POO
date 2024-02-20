2a195525830c4358548871bef9760f8a4cb71757
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2024-02-20 09:57:25 -0600

    Add files via upload

---
+++ EVA1_7_Television1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_7_television1;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA1_7_Television1 {
+
+    public static void main(String[] args) {
+        televison tv = new televison();
+       tv.imprimirconfiguracion();
+       tv.bajarCanal();
+       tv.bajarVolumen();
+       tv.power();
+       tv.imprimirconfiguracion();
+       tv.bajarCanal();
+       tv.bajarVolumen();
+       tv.power();
+       tv.imprimirconfiguracion();
+       tv.bajarCanal();
+       tv.bajarVolumen();
+       tv.power();
+    }
+}
