0620463c92f077ee3d8991413befad9b2ed7500d
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2024-02-22 10:28:11 -0600

    Add files via upload

---
+++ EVA1_9_TRIANGULO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_triangulo;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA1_TRIANGULO_ {
+
+    public static void main(String[] args) {
+        TRIANGULO tri = new TRIANGULO();
+        tri.setaltura(20);
+        tri.setbase(50);
+        tri.imprimirDatos();
+        
+    }
+}
