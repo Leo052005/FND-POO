f1810cd70922f28cef930ae669e800da8a88e551
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2024-05-21 17:34:25 -0600

    Add files via upload

---
+++ EVA3_1_EXEPCIONES/src/main/java/com/mycompany/eva3_1_exepciones/EVA3_1_EXEPCIONES.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_exepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA3_1_EXEPCIONES {
+
+    public static void main(String[] args) {
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Por favor, ingresa un número entero: "); 
+        int nume = scanner.nextInt();
+        
+        System.out.println("Has ingresado el número entero: " + nume);
+
+         }
+    }
+
